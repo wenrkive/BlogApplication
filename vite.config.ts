598404bbba55b6dc6c8aad18fb559199e82d87e3
Wine <wenrkive.dev@gmail.com>
2025-10-03 09:41:59 -0400

    Atualiza index.html

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
   import path from 'path';
 
   export default defineConfig({
-    base: "/BlogApplication/",
+    base: '/BlogApplication/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
